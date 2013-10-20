e574ac8a54d4867d23f4c38ea04dfaca68a7d5fa
Neil Ford <neil@neilcford.co.uk>
2013-10-20 17:11:07 +0100

    added initial python3 projects

---
+++ python3/blink3.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+red = 17
+green = 22
+yellow = 10
+led_pin = red
+
+print ("Setting up GPIO")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(red, GPIO.OUT)
+GPIO.setup(yellow, GPIO.OUT)
+GPIO.setup(green, GPIO.OUT)
+
+def enable_led(should_enable):
+	if should_enable:
+		GPIO.output(led_pin, True)
+	else:
+		GPIO.output(led_pin, False)
+
+
+for n in range(10):
+        enable_led(False)
+        led_pin = red
+        enable_led(True)
+        print ("red in ON")
+        sleep(2)
+        
+        enable_led(False)
+        led_pin = green
+        enable_led(True)
+        print("red is OFF, green is ON")
+        sleep(2)
+
+        enable_led(False)
+        led_pin = yellow
+        enable_led(True)
+        print("green is OFF, yellow is ON")
+        sleep(2)
+
+
+enable_led(False)
+print("End")
+GPIO.cleanup()
---
+++ python3/switch.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+# import modules
+import RPi.GPIO as GPIO
+from time import sleep
+import random
+
+# set-up variables
+red = 17
+green = 22
+yellow = 10
+led_pin = red
+switch_pin = 23
+
+# set-up GPIO states
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(red, GPIO.OUT)
+GPIO.setup(yellow, GPIO.OUT)
+GPIO.setup(green, GPIO.OUT)
+GPIO.setup(switch_pin, GPIO.IN)
+
+# enable_led function
+def enable_led(should_enable):
+	if should_enable:
+		GPIO.output(led_pin, True)
+	else:
+		GPIO.output(led_pin, False)
+		
+# main loop
+while True:
+    if (GPIO.input(switch_pin) == False):
+        print("Switch Pressed\n")
+
+        times = random.randint(3, 7)
+        print("time is ", times)
+
+        for n in range(times):
+            enable_led(False)
+            led_pin = red
+            enable_led(True)
+            print ("red in ON")
+            sleep(2)
+        
+            enable_led(False)
+            led_pin = green
+            enable_led(True)
+            print("red is OFF, green is ON")
+            sleep(2)
+
+            enable_led(False)
+            led_pin = yellow
+            enable_led(True)
+            print("green is OFF, yellow is ON")
+            sleep(2)
+
+
+        enable_led(False)
+        print("End\n")
+
+    sleep(0.5)
+    
+
